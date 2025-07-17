9cbae69a0c677a9885d9a4c899a9c5ffa3db732e
mahmoudalnmr <mahmoudalnmr1998@gmail.com>
2025-07-17 17:03:29 +0300

    enhanced error handling using ProblemDetails and unifying handling of not found errors

--- catalogservice/src/main/java/com/bookstore/catalogservice/web/controllers/GlobalExceptionHandler.java
+++ catalogservice/src/main/java/com/bookstore/catalogservice/web/controllers/GlobalExceptionHandler.java
@@ -3,30 +3,32 @@ package com.bookstore.catalogservice.web.controllers;
 import com.bookstore.catalogservice.domain.exceptions.PageNotFoundException;
 import com.bookstore.catalogservice.domain.exceptions.ProductNotFoundException;
 import org.hibernate.annotations.NotFound;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler
 {
-    @ExceptionHandler(PageNotFoundException.class)
-    public ResponseEntity<Map<String,String>> handlePageNotFound(PageNotFoundException ex)
+    private static final String SERVICE_NAME = "catalog-service";
+    private static final URI NOT_FOUND = URI.create("https://api.bookstore.com/errors/not-found");
+    @ExceptionHandler({PageNotFoundException.class, ProductNotFoundException.class})
+    public ProblemDetail handleNotFound(RuntimeException ex)
     {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(Map.of("error","page not found", "message",ex.getMessage()));
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problemDetail.setTitle("Not Found");
+        problemDetail.setType(NOT_FOUND);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
     }
 
-    @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<Map<String,String>> handleProductNotFound(ProductNotFoundException ex)
-    {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(Map.of("error", "Product Not Found","Message",ex.getMessage()));
-    }
 }
